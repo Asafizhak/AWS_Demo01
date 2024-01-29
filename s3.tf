533eb0aef430b383b7d54faad7533de8899bb757
Asaf Izhak <asaf.izhak@gmail.com>
2024-01-29 16:31:57 +0200

    s3 fix

--- s3.tf
+++ s3.tf
@@ -1,23 +1,20 @@
-# s3.tf
 resource "aws_s3_bucket" "my_s3_bucket" {
-  bucket = "my-s3-awsd-demo01-asafiz"  # Change to a unique name
-
+  bucket = "my-s3-aws-demo01-asafiz"
   #acl    = "private"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3_kms_key.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  tags = {
-    Name = "my-s3-bucket"
-  }
 }
 
 resource "aws_kms_key" "s3_kms_key" {
-  description             = "KMS key for S3 bucket"
-  deletion_window_in_days = 30
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
