474ed4403f4f94cca6db402a5c5d31c9e4c84b07
Asaf Izhak <asaf.izhak@gmail.com>
2024-01-29 14:52:50 +0200

    add files

---
+++ s3.tf
@@ -0,0 +1,23 @@
+# s3.tf
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-unique-s3-bucket-name"  # Change to a unique name
+
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.s3_kms_key.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name = "my-s3-bucket"
+  }
+}
+
+resource "aws_kms_key" "s3_kms_key" {
+  description             = "KMS key for S3 bucket"
+  deletion_window_in_days = 30
+}
